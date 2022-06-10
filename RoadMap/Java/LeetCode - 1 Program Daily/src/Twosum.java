6445730e29a0f7f63564e01dde1ae9dcd17109cb
kushalpuri <kkitlearning@gmail.com>
2022-06-10 16:35:39 -0500

    push the code

---
+++ RoadMap/Java/LeetCode - 1 Program Daily/src/Twosum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Twosum {
+
+
+
+    public static int[] findSum(int[] arrNums, int target){
+
+        HashMap<Integer,Integer> numsMap = new HashMap<>();
+
+        for(int i=0;i<arrNums.length;i++) {
+
+
+            Integer requiredNum =  (Integer)(target-arrNums[i]);
+
+            if(numsMap.containsKey(requiredNum)){
+                return new int[]{numsMap.get(requiredNum),i};
+            }
+                numsMap.put(arrNums[i],i);
+        }
+        return new int[]{-1 , -1};
+    }
+
+    public static void main(String[] args) {
+        int[] testArr = {1,2,3,4,5};
+        System.out.println(findSum(testArr,5)[0]+"=>"+findSum(testArr,5)[1]);
+    }
+}
