fa062bca43f405bac65f42ea031cb2c4087b9d9f
kushalpuri <kkitlearning@gmail.com>
2022-06-25 16:12:13 -0500

    Automation UI Code

--- RoadMap/Java/AutoFramework/src/test/java/org/selenium/TestCase.java
+++ RoadMap/Java/AutoFramework/src/test/java/org/selenium/TestCase.java
@@ -1,7 +1,9 @@
 package org.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +12,10 @@ public class TestCase {
     public void dummyTest(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        WebElement element = null;
 
         driver.get("http://google.com");
+        element.findElement(By.cssSelector("input[title='Search']")).sendKeys("fsfs");
         driver.quit();
     }
 }
